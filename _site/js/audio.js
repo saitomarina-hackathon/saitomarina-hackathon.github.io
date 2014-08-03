8d18171aace5244565aec85baebdd458fa8b1c17
ta9to <ta9to.kd@gmail.com>
2014-08-03 17:50:37 +0900

    fix audio

--- _site/js/audio.js
+++ _site/js/audio.js
@@ -1,5 +1,8 @@
 var play_src = "";
 $('.music').click(function(){
+  $('audio').each(function(key, audio){
+    audio.pause();
+  });
   var audio_element = $(this).find('audio');
   audio_element[0].play();
   if (play_src == audio_element.attr('src')) {
